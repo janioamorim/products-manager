06befd092c2784c9e3f5ac5d81f6fb03196102b4
Janio Amorim <janioamorim04@gmail.com>
2022-01-10 19:47:09 -0300

    file interface

---
+++ src/interfaces/interfaces.types.ts
@@ -0,0 +1,16 @@
+
+export interface IUsers {
+    id: number
+    name: string,
+    user: string,
+    pass: string,      
+}
+
+export interface IProduct {
+    id: number
+    name: string,
+    price: string,
+    packaging: string,
+    quantity: number,        
+}
+
